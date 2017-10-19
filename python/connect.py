d7ac688c3d178b3b478b5476dbb22014b588b5b1
Gabriela Muller <mullergabrielaa@gmail.com>
2017-10-19 11:01:23 +0000

    Second commit

---
+++ python/connect.py
@@ -0,0 +1,17 @@
+import pymysql
+import datetime
+
+connection = pymysql.connect(host='localhost',
+                            user='gabrielamuller',
+                            password='',
+                            db='Chinook')
+
+try:
+   with connection.cursor() as cursor:
+       data = [("bob", 21, "1990-02-06 23:04:56"),
+               ("jim", 56, "1955-05-09 13:12:45"),
+               ("fred", 100, "1911-09-12 01:01:01")]
+       cursor.executemany("insert into Friends values (%s,%s,%s)", data)
+       connection.commit()
+finally:
+   connection.close()
